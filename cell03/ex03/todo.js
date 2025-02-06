804c6a5658696a71707958929c84078879758301
nasitpoon <nasitpoon@gmail.com>
2025-02-06 13:47:41 +0700

    a

--- cell03/ex03/todo.js
+++ cell03/ex03/todo.js
@@ -1,49 +1,42 @@
-const list = document.getElementById("ft_list");
-let TodoList = [];
+const list = document.getElementById("ft_list")
+let TodoList = []
 
 function createTodo(text) {
-    const element = document.createElement('div');
-    element.classList.add('todo');
-    element.innerHTML = `<p>${text}</p> <button onclick="remove('${text}')">Delete</button>`;
+    const element = document.createElement('div')
+    element.classList.add('todo')
+    element.innerHTML = '<p>' + text + '</p>' + '<button onclick="remove(' + "'" + text + "'" + ')">Delete</button>'
     return element;
 }
 
 function render() {
-    list.innerHTML = '';
+    list.innerHTML = ''
     for (let index = 0; index < TodoList.length; index++) {
         const element = TodoList[index];
-        list.appendChild(createTodo(element));  // Append elements instead of using innerHTML
+        list.innerHTML += createTodo(element).outerHTML
     }
-    saveTodos();
+    document.cookie = JSON.stringify(TodoList)
 }
 
 function newTodo() {
-    let name = prompt("Name the todo.");
-    if (name && name.trim().length > 0) {
-        TodoList.unshift(name.trim());
-        render();
+    let name = prompt("Name the todo.")
+    if (name.length > 0) {
+        TodoList.unshift(name)
+        render()
     }
 }
 
 function remove(text) {
-    let yes = confirm('Are you sure you want to remove this?');
+    let yes = confirm('Are you sure to remove.')
     if (yes) {
-        TodoList = TodoList.filter(a => a !== text);
-        render();
+        TodoList = TodoList.filter((a) => a != text)
+        render()
     }
 }
 
-function saveTodos() {
-    document.cookie = `todos=${encodeURIComponent(JSON.stringify(TodoList))}; path=/; max-age=86400`;
-}
-
-function loadTodos() {
-    const cookies = document.cookie.split('; ');
-    const todoCookie = cookies.find(row => row.startsWith('todos='));
-    if (todoCookie) {
-        TodoList = JSON.parse(decodeURIComponent(todoCookie.split('=')[1]));
-        render();
+window.onload = function () {
+    let save = document.cookie;
+    if (save.length > 0) {
+        TodoList = JSON.parse(save)
+        render()
     }
 }
-
-window.onload = loadTodos;
